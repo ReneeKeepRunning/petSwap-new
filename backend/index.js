bcbae81e052eac017561d8b0bfa2d079739516fd
Ruiyi Tong <tongruiyi1997@gmail.com>
2026-03-24 11:52:55 +1100

    edit vercal version

--- backend/index.js
+++ backend/index.js
@@ -69,7 +69,10 @@ app.use(session({
 //   credentials: true
 // }));
 
-
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+  credentials: true
+}));
 
 
 app.use(helmet());
@@ -112,7 +115,16 @@ app.use(express.static('public', {
   maxAge: '30d'
 }));
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+// });
+
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-});
+  });
+}
+
+module.exports = app;
